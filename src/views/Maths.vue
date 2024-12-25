945d992ff449a96881867159e5f5ed28a5f624fb
Llegion404 <LLEGION40404@gmail.com>
2024-12-25 15:49:52 +0500

    starting maths

--- src/views/Maths.vue
+++ src/views/Maths.vue
@@ -3,8 +3,12 @@ import { NGradientText } from 'naive-ui';
 </script>
 
 <template>
-	<section class="container pt-5">
-		<h1>Do some <NGradientText type="success"> Maths </NGradientText></h1>
-		<main></main>
+	<section class="h-screen relative">
+		<div class="container z-20 pt-10">
+			<h1>Do some <NGradientText type="success"> Maths </NGradientText></h1>
+			<main></main>
+		</div>
+		<div
+			class="absolute top-0 bg-[url('https://i.pinimg.com/originals/fc/37/a8/fc37a832ba5fe504962181e7dcfa370c.jpg')] h-full w-full -z-50 opacity-5"></div>
 	</section>
 </template>
