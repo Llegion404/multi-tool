bbac1944c0ae5f06420cfcaf21edaf41d8fc91f0
Llegion404 <LLEGION40404@gmail.com>
2025-01-02 11:54:13 -0500

    component for gradient border

---
+++ src/components/GradientBorder.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+  width: {
+    type: String,
+    default: "full",
+  },
+  borderColor: {
+    type: String,
+    default: "[#52B69A]",
+  },
+  fromColor: {
+    type: String,
+    default: "transparent",
+  },
+  toColor: {
+    type: String,
+    default: "transparent",
+  },
+});
+</script>
+
+<template>
+  <div class="relative">
+    <slot></slot>
+    <div
+      :class="`absolute h-px bottom-0 left-0 right-0 bg-gradient-to-r from-${fromColor} via-${borderColor} to-${toColor} w-${props.width}`"></div>
+  </div>
+</template>
